48deee204bcc2a07ef2b5d7ebe6a52297923ba21
Krish Tiwary <krishna2may@gmail.com>
2023-09-06 23:34:23 +0530

    Add files via upload

---
+++ module.js
@@ -0,0 +1,10 @@
+/*Module is a file in node js besically incpuate the code */
+
+//How to use require in node js what is the basic logic
+//IIFE function provide the function require(Immediatly invoke funtion)
+
+
+
+const color = require('cli-color');
+console.log(color.red('Krish Tiwary'))
+console.log(color.blue('Krish Tiwary'))
